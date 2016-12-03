c0e56afacf333cf7e56e0db568f90579f37b0f54
maartjesteendam <maartjesteendam@gmail.com>
2016-12-03 10:48:38 +0100

    pietcontroller

---
+++ app/helpers/piets_helper.rb
@@ -0,0 +1,2 @@
+module PietsHelper
+end
--- app/models/piet.rb
+++ app/models/piet.rb
@@ -1,3 +1,11 @@
 class Piet < ApplicationRecord
   belongs_to :sint
+
+  def gender_to_s
+    if self.gender == true
+      "Male"
+    else
+      "Female"
+    end
+  end
 end
